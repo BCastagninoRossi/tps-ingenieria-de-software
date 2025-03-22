2b53eb916ce0a20def0e68d7403677d25808e41a
BCastagninoRossi <brcastagnino@gmail.com>
2025-03-22 18:15:42 -0300

    added function to be able to put pallets to the same city toghether

--- tp1/Route.hs
+++ tp1/Route.hs
@@ -8,9 +8,9 @@ newR :: [ String ] -> Route -- construye una ruta según una lista de ciudades
 newR ciudades = Rou ciudades
 
 inOrderR :: Route -> String -> String -> Bool -- indica si la primer ciudad consultada está antes que la segunda ciudad en la ruta
-inOrderR (Rou ciudades) ciudad1 ciudad2 =
-    ciudad1 `elem` ciudades && ciudad2 `elem` ciudades &&
-    foldr (\ciudad acc -> if ciudad == ciudad2 then False else ciudad == ciudad1 || acc) False ciudades
+inOrderR (Rou ciudades) ciudad1 ciudad2 | ciudad1 == ciudad2 && ciudad1 `elem` ciudades = True
+                                        | otherwise = ciudad1 `elem` ciudades && ciudad2 `elem` ciudades &&
+                                          foldr (\ciudad acc -> if ciudad == ciudad2 then False else ciudad == ciudad1 || acc) False ciudades
 
 inRouteR :: Route -> String -> Bool -- indica si la ciudad consultada está en la ruta
 inRouteR (Rou ciudades) ciudad = ciudad `elem` ciudades
